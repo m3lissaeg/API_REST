e3bc0e5ae9f22aed6774af7dd8c2e707c987a977
MelissaEscobar <melissa.escobar@utp.edu.co>
2020-12-10 11:05:42 -0500

    Final

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Models\Student;
 use Illuminate\Support\Facades\Route;
-
+use Symfony\Component\HttpFoundation\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,13 +18,39 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('students', 'StudentController@index');
-Route::get('students/{student}', 'StudentController@show');
-Route::post('students', 'StudentController@store');
-Route::put('students/{student}', 'StudentController@update');
-Route::delete('students/{student}', 'StudentController@delete');
-Route::post('register', 'Auth\RegisterController@register');
-Route::post('login', 'Auth\LoginController@login');
+Route::get('students', function() {
+    // If the Content-Type and Accept headers are set to 'application/json', 
+    // this will return a JSON structure. This will be cleaned up later.
+    return Student::all();
+});
+ 
+Route::get('students/{id}', function($id) {
+    return Student::find($id);
+});
+
+Route::post('students', function(Request $request) {
+    return Student::create($request->all);
+});
+
+Route::put('students/{id}', function(Request $request, $id) {
+    $article = Student::findOrFail($id);
+    $article->update($request->all());
+
+    return $article;
+});
+
+Route::delete('students/{id}', function($id) {
+    Student::find($id)->delete();
+
+    return 204;
+});
+// Route::get('students', 'StudentController@index');
+// Route::get('students/{student}', 'StudentController@show');
+// Route::post('students', 'StudentController@store');
+// Route::put('students/{student}', 'StudentController@update');
+// Route::delete('students/{student}', 'StudentController@delete');
+// Route::post('register', 'Auth\RegisterController@register');
+// Route::post('login', 'Auth\LoginController@login');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
